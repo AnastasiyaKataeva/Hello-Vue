aeee5b5f5007723e54eaa9bf56880ea93fbc593a
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2022-12-21 16:22:08 +0300

    Сoздано приложение vue cli. Работа с компонентами: 1.локальное и глобальное подключение компонентов 2. валидация  входящих параметров 4. передача параметров от родительского компонента 3. передача параметров от дочернего компонента

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>My car name: {{ carName }}</h1>
+    <app-car :car-name="carName" :car-year="carYear" @changeName="carName = $event"></app-car>
+  </div>
+</template>
+
+<script>
+import Car from "./Car.vue";
+export default {
+  name: 'app',
+  data () {
+    return {
+      carName: 'Mazda',
+      carYear: '2009'
+    }
+  },
+ components: {
+    appCar: Car,
+ }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Car.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <h1>Name: {{carName}}/{{reverseName}}</h1>
+  <h2>Year: {{carYear}}</h2>
+  <button @click="changeCar">Change car</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    carName: {
+      type: String,
+      required: true
+    },
+    carYear: {
+      type: Number
+    }
+  },
+  methods:{
+    changeCar() {
+      this.carName='Reno',
+       this.$emit('changeName', this.carName)
+    }
+  },
+  computed: {
+    reverseName(){
+      return this.carName.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
